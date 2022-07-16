3b1f79c17881b71d2918b301a89d6f5ccc60484b
muratbek2255 <muratbek2255@gmail.com>
2022-07-16 16:52:16 +0600

    create cafe app

---
+++ src/views/Home.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="home">
+    <h1 class="title">Welcome to Cafe</h1>
+  <nav>
+    <ul>
+      <li>Restaurant</li>
+      <li>Cafe</li>
+      <li>Fast-food</li>
+      <li>Bar</li>
+    </ul>
+  </nav>
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  components: {
+  }
+}
+</script>
